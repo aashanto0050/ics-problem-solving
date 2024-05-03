99443ae53519028c6c82d4f5774f09d8a2dd7f47
aashanto0050 <md.asrafulalam@Mds-MacBook-Air.local>
2024-05-04 02:10:26 +0600

    add

--- p3.c
+++ p3.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
+
 int main()
 {
-int num1;
- printf("Enter a Number:\n");
- scanf("%d",&num1);
- 
- if(num1%2==0 || num1==0)
-  {
-     printf("Even");
-  }
-  else printf("Odd");
-  
- return 0;
+    int n;
+    scanf("%d",&n);
+    if(n==0) printf("Zero");
+    else if(n==1) 
+    {printf("One");}
+    else if(n==2) 
+    {   
+        printf("Two");
+        }
+    else if(n==3) {printf("Three");}
+    else if(n==4) {printf("Four");}
+    else if(n==5) {printf("Five");}
+    else if(n==6) {printf("Six");}
+    else if(n==7) {printf("Seven");}
+    else if(n==8) {printf("Eight");}
+    else if(n==9) {printf("Nine");}
+    else printf("Invalid Input");
+
+
+    return 0;
 }
